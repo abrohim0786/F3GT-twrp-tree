c04b51647e22e008cca92137e968a1b46e3d2487
ツ๛abrohim๛ <callofduty.pubg786@gmail.com>
2025-08-15 09:47:21 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -67,7 +67,6 @@ TW_NO_SCREEN_BLANK := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.usb0/lun.%d/file
 TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_INCLUDE_NTFS_3G := true
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
