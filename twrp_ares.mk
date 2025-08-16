920ed1e675d1dd51ba1ca113bed1718c5cc67852
ツ๛abrohim๛ <callofduty.pubg786@gmail.com>
2025-08-16 14:44:50 +0530

    Update twrp_ares.mk

--- twrp_ares.mk
+++ twrp_ares.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2025 ツ๛abrohim๛ TWRP device tree
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,6 +8,9 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
@@ -22,3 +25,7 @@ PRODUCT_MODEL := Poco F3 GT,Redmi K40 Gaming
 PRODUCT_MANUFACTURER := xiaomi
 
 BUILD_FINGERPRINT := POCO/vnd_ares/ares:12/SP1A.210812.016/V816.0.2.0.TKJINXM:user/release-keys
+
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31
