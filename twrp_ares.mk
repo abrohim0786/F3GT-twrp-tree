c0e6965c81df82fe21537a26cedfc74a6c7071c0
ツ๛abrohim๛ <callofduty.pubg786@gmail.com>
2025-08-16 12:26:49 +0530

    Update twrp_ares.mk

--- twrp_ares.mk
+++ twrp_ares.mk
@@ -9,9 +9,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
 
@@ -25,7 +22,3 @@ PRODUCT_MODEL := Poco F3 GT,Redmi K40 Gaming
 PRODUCT_MANUFACTURER := xiaomi
 
 BUILD_FINGERPRINT := POCO/vnd_ares/ares:12/SP1A.210812.016/V816.0.2.0.TKJINXM:user/release-keys
-
-# HACK: Set vendor patch level
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2099-12-31
